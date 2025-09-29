bb3cca0363ae48c1b5cdd99a54aa80d0515eae2d
Harounjlassi <jlassi.Haroun@esprit.tn>
2025-09-29 12:26:32 +0100

    update:1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+// Created by Haroun Jlassi
+// React Tic Tac Toe Project - 2025
+
 import { useState } from "react";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import GameBoard from "./components/GameBoard";
 import GameOver from "./components/GameOver";
 import { WINNING_COMBINATIONS } from "./components/winning-combinations.js";
-const initialGameBoard = [
+const PLAYERS={
+    X: "Player 1",
+    O: "Player 2",
+  }
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -17,18 +24,9 @@ function derivedActivePlayer(gameTurns) {
   }
   return currentPlayer;
 }
-function App() {
-  const [players,sePlayers]=useState({
-    X:'Player 1',
-    O:'Player 2'
-  })
-  //const [activePlayer, setActivePlayer] = useState("X");
-  const [gameTurns, setGameTurns] = useState([]);
-  //const [hasWinner, setHasWinner] = useState(false);
-
-  let activePlayer = derivedActivePlayer(gameTurns);
-  //We need to crete a copy:deep copy
-  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+function deriveGmaeBoard(gameTurns) {
+   //We need to crete a copy:deep copy
+  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
   //deriving state
   //gameBoard is computed value that is devived from a state
   for (const turn of gameTurns) {
@@ -36,7 +34,11 @@ function App() {
     const { row, col } = square;
     gameBoard[row][col] = player;
   }
+  return gameBoard;
+}
+function deriveWinner(gameBoard, players) {
   let winner;
+
   for (const combinatios of WINNING_COMBINATIONS) {
     const firstSquareSymbol =
       gameBoard[combinatios[0].row][combinatios[0].column];
@@ -52,6 +54,19 @@ function App() {
       winner = players[firstSquareSymbol];
     }
   }
+  return winner;
+}
+function App() {
+  const [players, sePlayers] = useState(PLAYERS);
+  //const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+  //const [hasWinner, setHasWinner] = useState(false);
+
+  let activePlayer = derivedActivePlayer(gameTurns);
+  const gameBoard = deriveGmaeBoard(gameTurns);
+ 
+  const winner = deriveWinner(gameBoard, players);
+
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -72,22 +87,31 @@ function App() {
   function handleRestart() {
     setGameTurns([]);
   }
-  function handlePlayerNameChange(symbol,newName) {
-    sePlayers(
-      prev=>{return {
+  function handlePlayerNameChange(symbol, newName) {
+    sePlayers((prev) => {
+      return {
         ...prev,
-        [symbol]:newName
-      }}
-    );
+        [symbol]: newName,
+      };
+    });
   }
-  
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} onChangeName={handlePlayerNameChange} />
-          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} onChangeName={handlePlayerNameChange}/>
+          <Player
+            name={PLAYERS.X}
+            symbol="X"
+            isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            name={PLAYERS.O}
+            symbol="O"
+            isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
         {(winner || hasDraw) && (
           <GameOver winner={winner} onRestart={handleRestart} />
@@ -95,7 +119,11 @@ function App() {
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
+       <footer style={{ marginTop: "20px", textAlign: "center", fontSize: "14px" }}>
+      © {new Date().getFullYear()} Haroun Jlassi
+    </footer>
     </main>
+    
   );
 }
 
